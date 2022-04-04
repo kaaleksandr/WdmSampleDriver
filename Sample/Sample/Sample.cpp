7d1e2d3053ce03cd8db32fd52ab41120b5e8c821
kaaleksandr <kaaleksandr@internet.ru>
2022-04-05 01:56:22 +0300

    add output version to dbglog

--- Sample/Sample/Sample.cpp
+++ Sample/Sample/Sample.cpp
@@ -1,11 +1,33 @@
 #include <ntddk.h>
 
-
-NTSTATUS
-DriverEntry(_In_ PDRIVER_OBJECT DriverOBject, _In_ PUNICODE_STRING RegistryPath)
+void SampleUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
-	UNREFERENCED_PARAMETER(DriverOBject);
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("Sample driver Unload called\n"));
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
 	UNREFERENCED_PARAMETER(RegistryPath);
 
-	return STATUS_SUCCESS;
+	DriverObject->DriverUnload = SampleUnload;
+
+	RTL_OSVERSIONINFOW info;
+	NTSTATUS status = RtlGetVersion(&info);
+	if (NT_SUCCESS(status))
+	{
+		KdPrint(("Sample driver initialized successfully\n"));
+		KdPrint(("[MY] Major=%u, Minor=%u, Build=%u",
+			info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber));
+	}
+	else
+	{
+		KdPrint(("Sample driver initialized failed\n"));
+		KdPrint(("[MY] RtlGetVersion returned %08X", status));
+	}
+
+	return status;
 }
